ca0561dff1921d160450c8f7d406dd3cee9a5979
Sophie Student <l_raffin@hotmail.com>
2023-02-08 18:44:51 +0000

    Added style checking

---
+++ HelloWorldStyle.java
@@ -0,0 +1,19 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Ms Raffin
+* @version 1.0
+* @since   2023-02-08
+*/
+public class HelloWorldStyle {
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world! with style");
+    }
+}
